3d8c82fcce9da41d6e9401c3c18ed220d21d8b52
Sven Brettschneider <Sven Brettschneider>
2016-03-08 11:10:38 +0100

    falende structuur assesment WEBS5

---
+++ models/book.js
@@ -0,0 +1,49 @@
+function init(mongoose){
+	console.log('Iniializing books schema');
+
+	var Schema = mongoose.Schema;
+	/*
+	TODO: Validation
+	- Title: Verplicht, String
+	- PublishDate: Verplicht, Date, voor vandaag
+	- Category: Verplicht, String
+	- Chapters: Array van JSNON { title, numberOfPages }
+	*/
+	var book = new Schema({
+		name: { type: String, required: true },
+		publishDate: {
+			type: Date,
+			required: true,
+			validate:
+			{
+				validator: function(v){
+					return v < new Date();
+				}, message:'{VALUE} is not a date before today'
+			}
+		},
+		category: {type: String, required: true},
+		chapters: {type: [{title: {type: String}, numberOfPages: {type: Number}}]},
+	});
+
+	book.virtual('totalNumberOfPages').get(function () {
+		var total = 0;
+		this.chapters.forEach(function(chapter){
+			total += chapter.numberOfPages;
+		});
+  return total;
+	});
+
+	book.set('toJSON', { virtuals: true });
+
+	mongoose.model('Book', book);
+
+	/*
+	TODO:
+	- De benodigde virtuals (Onder andere totalNumberOfPages, opgebouwd uit numberOfPages van chapters)
+	- De benodigde extra validation
+	- De benodigde static methods
+	- De benodigde instance methods
+	*/
+}
+
+module.exports = init;
---
+++ routes/books.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express();
+var Book;
+var _ = require('underscore');
+var async = require('async');
+
+/*
+	TODO:
+	- QueryString filter: topCategories={nummer}
+		Tel alle boeken in een categorie
+		Order deze categorie van meeste naar minste boeken
+		Geef alleen de boeken terug die in de top {nummer} categorieën voorkomen
+		(For now: Een boek kan maar 1 categorie hebben)
+
+	// Ten slotte, een moeilijkere (door Async methodes)
+	- Population: Vul alle autors van het boek
+*/
+function getBooks(req, res){
+	var query = Book.find();
+
+	query.exec(function (err, data) {
+		res.json(data);
+	});
+
+	if(req.params.id){
+		query._id = req.params.id.toLowerCase();
+	}
+
+	if(req.params.id){
+		data = data[0];
+	}
+
+}
+
+// Routing
+router.route('/')
+	.get(getBooks);
+
+router.route('/:id')
+	.get(getBooks);
+
+// Export
+module.exports = function (mongoose){
+	console.log('Initializing books routing module');
+	Book = mongoose.model('Book');
+	return router;
+};
